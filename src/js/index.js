db65fb683e4bebb97bb0ab87c923648541ddfbe1
Tim Johnson <tjohnson@scitent.com>
2016-12-31 04:45:30 -0500

    updated production bundle

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import url from 'url';
 
 // $(document).ready(() => {
 // document.body.innerHTML = JSON.stringify(url.parse(window.location.href));
-console.log('something works', JSON.stringify(url.parse(window.location.href)));
+console.log('something works', JSON.stringify(url.parse(window.location.href, true, true)));
 // });
 
 export default 'index';
